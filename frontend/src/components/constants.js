123d779bdd063cc690ea9b5ef05781b5c607bd3f
BINAH25 <visteen192@gmail.com>
2025-04-02 09:25:09 +0000

    docker file updated

--- frontend/src/components/constants.js
+++ frontend/src/components/constants.js
@@ -1,1 +1,1 @@
-export const BASE_API_URI = 'http://frontend-cluster-alb-923969298.eu-west-1.elb.amazonaws.com';
+export const BASE_API_URI = 'http://frontend-cluster-alb-410993149.eu-west-1.elb.amazonaws.com';
